e31738deeadf84a7a674b21c0f43a8e9aacc04ef
henrique <henrique.pedasi@gmail.com>
2019-07-22 23:38:15 -0300

    sending first commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      {/** Switch garante que apenas uma pagina seja exibica */}
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository/:repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
