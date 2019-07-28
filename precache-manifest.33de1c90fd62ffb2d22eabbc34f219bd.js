b0f19ec39244b593480cf21ee6d5ccf8eba62ffc
henrique <henrique.pedasi@gmail.com>
2019-07-28 19:01:49 -0300

    Updates

---
+++ precache-manifest.33de1c90fd62ffb2d22eabbc34f219bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84458126cae72c4876eedb54a46698e5",
+    "url": "/github-repositories-search/index.html"
+  },
+  {
+    "revision": "b613da842a09ba84eddb",
+    "url": "/github-repositories-search/static/js/2.05bef0a4.chunk.js"
+  },
+  {
+    "revision": "327d956b612492006cdf",
+    "url": "/github-repositories-search/static/js/main.b0a09de3.chunk.js"
+  },
+  {
+    "revision": "854012236cc7c9aab14b",
+    "url": "/github-repositories-search/static/js/runtime~main.db399d4f.js"
+  }
+]);
